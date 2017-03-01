31bda23959d3d40f16b73f8596ad9510c3d29c29
Annika Backstrom <annika@sixohthree.com>
2017-03-01 12:14:02 -0500

    Automatic commit of index.txt

--- push.sh
+++ push.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# synology diskstation isn't nice about setting the environment in scheduled tasks
+HOME=/var/services/homes/annika
+
+# update index.txt from dropbox
 cd /volume1/annika/literally.pictures
 cp /volume1/sync/dropbox/Notes/literally.pictures.txt /volume1/annika/literally.pictures/index.txt
+
+# push to remote repository
 git add index.txt
 git commit -am 'Automatic commit of index.txt'
 git push origin HEAD
