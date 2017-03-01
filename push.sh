4543ab5fa533d07bbff7b9ac8cac3dba3a423b18
Annika Backstrom <annika@sixohthree.com>
2017-03-01 17:18:17 +0000

    pull before push

--- push.sh
+++ push.sh
@@ -3,8 +3,13 @@
 # synology diskstation isn't nice about setting the environment in scheduled tasks
 HOME=/var/services/homes/annika
 
-# update index.txt from dropbox
 cd /volume1/annika/literally.pictures
+
+# update local copy
+git fetch
+git reset --hard origin/master
+
+# update index.txt from dropbox
 cp /volume1/sync/dropbox/Notes/literally.pictures.txt /volume1/annika/literally.pictures/index.txt
 
 # push to remote repository
